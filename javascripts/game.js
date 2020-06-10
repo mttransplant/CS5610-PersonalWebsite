dfad8e49d0ce10c7faf149b0b2bdb8175844bb05
Adam Smith <apsmith@ccs.neu.edu>
2020-06-10 13:40:29 -0400

    Completed functionality of game

--- javascripts/game.js
+++ javascripts/game.js
@@ -3,50 +3,55 @@
 
 let target = $('#target'),
     gameBoard = $('#game_board'),
+    success = $('#success'),
+    restart = $('#restart'),
     targetHeight = target.height(),
     targetWidth = target.width(),
-    the_game = 0,
+    targetRadius = targetHeight / 2,
+    gameOver = false,
     anim_id = 0;
 
-$(function() {
-    the_game = function() {
+$(gameBoard).mousemove(function(e) {
+    if (!gameOver && e.buttons === 0) {
+        offset = $(gameBoard).offset();
+        mouseX = e.pageX - offset.left;
+        mouseY = e.pageY - offset.top;
+        // console.log(`Mouse is at: {${mouseX}, ${mouseY}}`);
+        targetX = parseInt(target.css('left')) + targetRadius;
+        targetY = parseInt(target.css('top')) + targetRadius;
+        // console.log(`Target is at: {${targetX}, ${targetY}}`);
 
-        if (check_near_target()) {
+        if (check_near_target(mouseX, mouseY, targetX, targetY)) {
             move_target();
         }
-
-        if (check_caught_target()) {
-            stop_the_game();
-            console.log("You won!! Congrats!!")
-        } else {
-            anim_id = requestAnimationFrame(the_game);
-        }
-    };
-
-    anim_id = requestAnimationFrame(the_game);
+    }
 });
 
-$("#stop").click(stop_the_game);
+$(target).click(stop_the_game);
 
-function check_near_target() {
-    myStatus = Math.floor(Math.random() * 10) === 0;
-    console.log(myStatus);
-    return myStatus;
-}
+$(restart).click(restart_the_game);
 
-function check_caught_target() {
-    return false;
+function check_near_target(mx, my, tx, ty) {
+    distance = Math.sqrt(Math.pow(mx - tx, 2) + Math.pow(my - ty, 2));
+    // console.log(`Distance to target center: ${distance}`);
+    // console.log(`Distance to target edge: ${distance - targetRadius}`);
+    return distance - targetRadius <= 0;
 }
 
 function move_target() {
     newY = Math.floor(Math.random() * (gameBoard.height() - targetHeight));
     newX = Math.floor(Math.random() * (gameBoard.width() - targetWidth));
-    console.log(`New X: ${newX} New Y: ${newY}`);
+    // console.log(`New X: ${newX} New Y: ${newY}`);
     target.css('top', newY);
     target.css('left', newX);
 }
 
 function stop_the_game() {
-    cancelAnimationFrame(anim_id);
-    target.css('background-color', 'red');
+    gameOver = true;
+    success.css('visibility', 'visible');
+}
+
+function restart_the_game() {
+    gameOver = false;
+    success.css('visibility', 'hidden');
 }
